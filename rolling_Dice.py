11159a2296cf8e7b69ab58880c7a95c34421c7df
andreimagallanes77-code <andreimagallanes77@gmail.com>
2026-03-03 00:44:25 +0800

    I missed type text as txt in line 15

--- rolling_Dice.py
+++ rolling_Dice.py
@@ -1,1 +1,21 @@
-print("andrei")
+print("Welcome to Rolling Dice")
+import tkinter as tk
+import random
+
+root = tk.Tk()
+root.title("Rolling Dice")
+root.geometry("400x400")
+root.configure(backround="#1e1e2f")
+
+total_score = 0
+rolling = False
+
+dice_faces = ["⚀", "⚁", "⚂", "⚃", "⚄", "⚅"]
+
+title_label = tk.Label(root, txt="Dice Roller",
+                       font=("Arial", 24, "bold"),
+                       bg="#1e1e2f", fg="white")
+title_label.pack(pady=20)
+
+dice_label = tk.Label(root, text="⚀",
+                      font=("Arial", 120)
