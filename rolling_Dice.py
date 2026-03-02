6b22183c588a9dbdd5de8bdfca3e68e984488d02
andreimagallanes77-code <andreimagallanes77@gmail.com>
2026-03-03 01:18:34 +0800

    I need to rest

--- rolling_Dice.py
+++ rolling_Dice.py
@@ -35,5 +35,29 @@ total_label.pack(pady=10)
 def animate_roll(count=10):
     global total_score, rolling
 
+    if count > 0:
+        random_face = random.choice(dice_faces)
+        dice_label.config(text=random_face)
+        root.after(100, animate_roll, count - 1)
 
+    else:
+        roll = random.randint(1, 6)
+        dice_label.config(text=dice_faces[roll])
+        result_label.config(text=f"You rolled: {roll}")
+        total_score += "roll"
+        total_label.config(text=f"Total: {total_score}")
+        rolling = "False"
+
+    def roll_dice():
+        global rolling
+        if not rolling:
+            rolling = True
+            animate_roll()
+
+    def reset_game():
+        global total_score
+        total_score = 0
+        total_label.config(text="Total: 0")
+        result_label.config(text="Press Roll!")
+        dice_label.config(text="⚀")
 
